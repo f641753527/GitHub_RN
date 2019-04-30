f9398961f80eb5db8a2c1ab7fd4b25e1664aa15d
f641753527 <641753527@qq.com>
2019-04-30 17:58:43 +0800

    logger 中间件添加

--- src/redux/action/theme/index.js
+++ src/redux/action/theme/index.js
@@ -1,7 +1,5 @@
 import * as Types from './types.js';
 
-console.log(Types);
-
 export function UPDATE_THEME(theme) {
   return {
     type: Types.UPDATE_THEME,
--- src/redux/index.js
+++ src/redux/index.js
@@ -3,8 +3,17 @@ import reducers from './reducer';
 import { middleware } from '../navigator/AppContainer';
 
 
+const logger = store => next => action => {
+    console.log('previous state', store.getState());
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    return result;
+}
+
 const middlewares = [
     middleware,
+    logger
 ];
 /**
  * 创建store
